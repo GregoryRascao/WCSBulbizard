9d699f686ce89838fa0f3e29f199ad82652e848e
litundinu <litundinu82@gmail;com>
2019-10-21 09:59:12 +0200

    Mon JS

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const navig = document.getElementById("nav-top");
 
-window.addEventListener("scroll", event =>{
-    navig.classList.add("new");
-});
+// Dinu START
+// 1 - Click on "Formateur"
+        
+// 2 - Find position of formateur on the page
+//  function position(test) {
+// var test = document.getElementById("Wild");
+// console.log(test.offsetTop)
+//  } 
+// position(); 
+ 
+// 3 - Scroll the window to this postion with javascript
+function scrollFunction1() {
+    window.scrollTo(0, 820);}
+
+function scrollFunction2() {
+    window.scrollTo(0, 1444);}
+
+function scrollFunction3() {
+    window.scrollTo(0, 5000);}
+function scrollFunction4() {
+    window.scrollTo(0, 400,);}
+//Dinu Fin
